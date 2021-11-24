2677f904b2124004612bcea36ecf05130be87a20
Ksathwik03 <iit2020241@iiita.ac.in>
2021-11-24 20:25:11 +0530

    initial commit

---
+++ src/Reg.java
@@ -0,0 +1,266 @@
+
+import java.awt.Color;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Reg extends javax.swing.JFrame {
+    public Connection c = null;
+    String CreateSql = null;
+   
+    public Reg() {
+        initComponents();
+       password.setBackground(new Color(0 , 0 , 0,64));
+       username.setBackground(new Color(0 , 0 , 0,64));
+       password.setOpaque(false);
+       username.setOpaque(false);
+    }
+    
+    public boolean register(String username,String password){
+         try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres","postgres", "postgres");
+        
+        PreparedStatement st = c.prepareStatement("SELECT * FROM public.users WHERE username = ?");
+        st.setString(1, username);
+        st.execute();
+        ResultSet r1=st.executeQuery();
+        if(r1.next()){
+            JOptionPane.showMessageDialog(null, "The username is already in use please enter a different username");
+            return false;
+        }
+        st = c.prepareStatement("SELECT COUNT(*) FROM public.users");
+        r1=st.executeQuery();
+        r1.next();
+        int id = r1.getInt(1)+2;
+        PreparedStatement stmt = c.prepareStatement("INSERT INTO users(id, username, password) VALUES(?, ?, ?)");
+        stmt.setInt(1, id);
+        stmt.setString(2, username);
+        stmt.setString(3, password);
+        stmt.executeUpdate();
+
+        c.close();
+      } catch (Exception e) {
+         e.printStackTrace();
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+         return true;
+    } 
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        Password = new javax.swing.JLabel();
+        password = new javax.swing.JTextField();
+        Register = new javax.swing.JButton();
+        Username = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        username = new javax.swing.JTextField();
+        Login = new javax.swing.JButton();
+        label1 = new java.awt.Label();
+        jLabel3 = new javax.swing.JLabel();
+
+        jLabel2.setText("jLabel2");
+
+        jButton2.setText("jButton2");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
+
+        jButton3.setText("jButton3");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(null);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel2.setBackground(new java.awt.Color(0, 0, 204));
+        jPanel2.setBorder(new javax.swing.border.MatteBorder(null));
+        jPanel2.setToolTipText("");
+        jPanel2.setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setText("-----------REGISTRATION-----------");
+        jPanel2.add(jLabel1);
+        jLabel1.setBounds(130, 40, 418, 94);
+
+        Password.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
+        Password.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Password.setText("PASSWORD");
+        jPanel2.add(Password);
+        Password.setBounds(20, 260, 110, 30);
+
+        password.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        password.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passwordActionPerformed(evt);
+            }
+        });
+        jPanel2.add(password);
+        password.setBounds(240, 260, 290, 40);
+
+        Register.setText("REGISTER");
+        Register.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RegisterActionPerformed(evt);
+            }
+        });
+        jPanel2.add(Register);
+        Register.setBounds(140, 360, 125, 44);
+
+        Username.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
+        Username.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        Username.setText("USERNAME");
+        jPanel2.add(Username);
+        Username.setBounds(20, 190, 130, 30);
+        jPanel2.add(jLabel6);
+        jLabel6.setBounds(70, 280, 0, 0);
+
+        username.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        username.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                usernameActionPerformed(evt);
+            }
+        });
+        jPanel2.add(username);
+        username.setBounds(240, 180, 290, 40);
+
+        Login.setText("Login");
+        Login.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LoginActionPerformed(evt);
+            }
+        });
+        jPanel2.add(Login);
+        Login.setBounds(320, 340, 120, 60);
+
+        label1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        label1.setText("label1");
+        label1.setVisible(false);
+        jPanel2.add(label1);
+        label1.setBounds(590, 130, 34, 20);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Book_Cover_Mockup_high_res.jpg"))); // NOI18N
+        jLabel3.setText("jLabel3");
+        jPanel2.add(jLabel3);
+        jLabel3.setBounds(0, -10, 710, 580);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 695, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 566, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void RegisterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RegisterActionPerformed
+
+        String Username_text =  username.getText(), Password_text = password.getText();
+        if(("".equals(Password_text)) || ("".equals(Username_text)))
+        {
+            System.out.println();
+            JOptionPane.showMessageDialog(null, "Please enter all the fields");
+        }
+        else
+        {
+            if(register(Username_text, Password_text)){
+            dispose();
+            E_magazine e=new E_magazine();
+            e.setVisible(true);
+            }
+        }
+
+    }//GEN-LAST:event_RegisterActionPerformed
+
+    private void usernameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_usernameActionPerformed
+    
+    }//GEN-LAST:event_usernameActionPerformed
+
+    private void passwordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordActionPerformed
+
+    }//GEN-LAST:event_passwordActionPerformed
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void LoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LoginActionPerformed
+        // TODO add your handling code here:
+        dispose();
+        new Login().setVisible(true);
+        
+//        new Login.setVisible(true);
+    }//GEN-LAST:event_LoginActionPerformed
+
+    public static void main(String args[]) {
+      
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Reg.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Reg.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Reg.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Reg.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+               new Reg().setVisible(true);
+     
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Login;
+    private javax.swing.JLabel Password;
+    private javax.swing.JButton Register;
+    private javax.swing.JLabel Username;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private java.awt.Label label1;
+    private javax.swing.JTextField password;
+    private javax.swing.JTextField username;
+    // End of variables declaration//GEN-END:variables
+}
